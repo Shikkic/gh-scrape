f7764ab560ae59cde67d20bebc0d4e0fcce8b865
Leonard Schuetz <leni.schuetz@me.com>
2016-06-06 13:14:04 +0200

    Fixed scrapeContributionStatsAndData method and added test

--- index.js
+++ index.js
@@ -43,31 +43,71 @@ exports.scrapeContributionStats = function (url, callback) {
 exports.scrapeContributionDataAndStats = function(url, callback) {
     var returnObj = {};
     getRequest(url, function(html) {
-        scrapeContributionStats(html, function(statsData) {
-            // TODO implement better error handling
-            if (statsData == null) {
-                getRequest(url, function(html) {
-                    scrapeContributionStats(html, function(statsData) {
-                        scrapeContributionData(html, function(contributionData) {
-                            returnObj = formatReturnData(contributionData, statsData);   
-                            callback(returnObj);
-                        });
-                    });
-                });
-            } else {
-                scrapeContributionData(html, function(contributionData) {
-                    returnObj = formatReturnData(contributionData, statsData); 
-                    callback(returnObj);
-                });
-            }
+        scrapeContributionData(html, function(contributionData) {
+            returnObj = formatReturnData(contributionData, deriveContributionStats(contributionData));
+            callback(returnObj);
         });
     });
 };
 
 // Helper functions
 
+function deriveContributionStats(contributionData) {
+
+    // Some variables
+    var longestStreak = 0;
+    var currentStreak = 0;
+    var lastDay = '0-0-0';
+
+    // Reduce the total number of contributions to a single integer
+    const totalContributions = contributionData.reduce(function (last, current, index) {
+
+        // Calculate the exptected day to continue the streak
+        var expectedDay = new Date(lastDay);
+        expectedDay.setDate(expectedDay.getDate() + 1); // Increment day by 1
+
+        const year = expectedDay.getFullYear();
+        var month = expectedDay.getMonth() + 1; // The month starts from zero
+        var day = expectedDay.getDate();
+
+        // Left-pad for noobs
+        if (month < 10) { month = '0' + month; }
+        if (day < 10) { day = '0' + day; }
+
+        expectedDay = (
+            year + '-' +
+            month + '-' +
+            day
+        );
+
+        // If the streak was continued, increment, else reset
+        if (expectedDay === current.dataDate) {
+            currentStreak++;
+        } else {
+            currentStreak = 0;
+        }
+
+        if (currentStreak > longestStreak) {
+            longestStreak = currentStreak;
+        }
+
+        // Update the last day
+        lastDay = current.dataDate;
+
+        return last + current.dataContributionCount;
+    }, 0);
+
+    return {
+        totalContributions: totalContributions,
+        longestStreak: longestStreak,
+        currentStreak: currentStreak
+    };
+}
+
+// Helper functions
+
 function formatReturnData(contributionData, statsData) {
-    var commitsToday = getCommitsToday(contributionData); 
+    var commitsToday = getCommitsToday(contributionData);
     var returnData = {contributionData: contributionData, statsData: statsData, commitsToday: commitsToday};
     return commitsToday == null ? null : returnData;
 };
@@ -119,7 +159,7 @@ function scrapeContributionStats(html, callback) {
         // If it fails validation return null
         callback(null);
     }
-}; 
+};
 
 // Returns an Object Containing Contribution Data
 // CommitDataObj = {
@@ -141,7 +181,7 @@ function scrapeContributionData(html, callback) {
             commitDataObj.dataContributionCount = dataContributionCount;
             commitDataObj.dataDate = dataDate;
             commitDataArray.push(commitDataObj);
-        } 
+        }
     });
 
     // Validate it contains data before sending
@@ -151,9 +191,9 @@ function scrapeContributionData(html, callback) {
         // If it fails validation return null
         callback(null);
     }
-}; 
+};
 
-// GET Request Helper Function 
+// GET Request Helper Function
 function getRequest(gitUrl, callback) {
     var options = {
         url: gitUrl
@@ -212,7 +252,7 @@ getRequest("http://www.github.com/skeswa", function(html) {
 			getRequest("http://www.github.com/skeswa", function(html) {
 				scrapeContributionStats(html, function(statsData) {
 					scrapeContributionData(html, function(contributionData) {
-						var returnObj = formatReturnData(contributionData, statsData);   
+						var returnObj = formatReturnData(contributionData, statsData);
                         console.log(returnObj);
 					});
 				});
@@ -222,7 +262,7 @@ getRequest("http://www.github.com/skeswa", function(html) {
             console.log(statsData);
 			scrapeContributionData(html, function(contributionData) {
                 console.log(contributionData);
-				var returnObj = formatReturnData(contributionData, statsData); 
+				var returnObj = formatReturnData(contributionData, statsData);
                 console.log(returnObj);
 			});
 		}
--- test.js
+++ test.js
@@ -1,44 +1,8 @@
 const gh = require('./index');
 gh.scrapeContributionDataAndStats('https://github.com/KCreate', (data) => {
-
-    let longestStreak = 0;
-    let lastDay = '0-0-0';
-
-    // Reduce the total number of contributions to a single integer
-    const totalContributions = data.contributionData.reduce((last, current, index) => {
-
-        // Calculate the exptected day to continue the streak
-        let expectedDay = new Date(lastDay);
-        expectedDay.setDate(expectedDay.getDate() + 1); // Increment day by 1
-
-        const year = expectedDay.getFullYear();
-        let month = expectedDay.getMonth() + 1; // The month starts from zero
-        let day = expectedDay.getDate();
-
-        // Left-pad for noobs
-        if (month < 10) { month = '0' + month; }
-        if (day < 10) { day = '0' + day; }
-
-        expectedDay = (
-            year + '-' +
-            month + '-' +
-            day
-        );
-        console.log(expectedDay, current.dataDate);
-
-        // If the streak was continued, increment, else reset
-        if (expectedDay === current.dataDate) {
-            longestStreak++;
-        } else {
-            longestStreak = 0;
-        }
-
-        // Update the last day
-        lastDay = current.dataDate;
-
-        return last + current.dataContributionCount;
-    }, 0);
-
-    console.log('total contributions: ' + totalContributions);
-    console.log('longest streak: ' + longestStreak);
+    if (Object.keys(data.statsData).length > 0) {
+        console.log('[ OK ] Test passed!');
+    } else {
+        console.log('[ FAIL ] Test failed!');
+    }
 });
